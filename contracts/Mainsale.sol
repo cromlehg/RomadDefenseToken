e02d00f7c5071555654e5c4b0b4a20ff8500ff08
natyur <natalia.yurkovets@gmail.com>
2018-02-13 00:13:08 +0300

    calculateTokens counts milestone bonuses now

--- contracts/Mainsale.sol
+++ contracts/Mainsale.sol
@@ -32,7 +32,12 @@ contract Mainsale is StagedCrowdsale, RobustCoinCommonSale {
   function calculateTokens(uint _invested) internal returns(uint) {
     uint milestoneIndex = currentMilestone(start);
     Milestone storage milestone = milestones[milestoneIndex];
-    return _invested.mul(price).div(1 ether);
+    
+    uint tokens = _invested.mul(price).div(1 ether);
+    if(milestone.bonus > 0) {
+      tokens = tokens.add(tokens.mul(milestone.bonus).div(percentRate));
+    }
+    return tokens;
   }
 
   function finish() public onlyOwner {
