54464228f21e0c9618064754af1b8c655b1c4229
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-05-22 14:07:27 +0300

    chore(test): update refundable test for presale

--- test/presale/refundable.js
+++ test/presale/refundable.js
@@ -44,11 +44,13 @@ export default function (Token, Crowdsale, wallets) {
 
   it('should deny refunds before end', async function () {
     await crowdsale.sendTransaction({value: ether(1), from: wallets[3]});
+    await crowdsale.approveCustomer(wallets[3], {from: wallets[1]});
     await crowdsale.refund({from: wallets[3]}).should.be.rejectedWith(EVMRevert);
   });
 
   it('should deny refunds after end if goal was reached', async function () {
     await crowdsale.sendTransaction({value: this.softcap, from: wallets[3]});
+    await crowdsale.approveCustomer(wallets[3], {from: wallets[1]});
     await increaseTimeTo(this.afterEnd);
     await crowdsale.refund({from: wallets[3]}).should.be.rejectedWith(EVMRevert);
   });
@@ -64,6 +66,18 @@ export default function (Token, Crowdsale, wallets) {
     await crowdsale.refund({from: wallets[3], gasPrice: 0}).should.be.fulfilled;
     const post = web3.eth.getBalance(wallets[3]);
     post.minus(pre).should.be.bignumber.equal(investment);
+  });
+
+  it('should allow refunds for unapproved customers after end if goal was reached', async function () {
+    await crowdsale.sendTransaction({value: this.softcap, from: wallets[3]});
+    await crowdsale.sendTransaction({value: ether(100), from: wallets[4], gasPrice: 0});
+    const pre = web3.eth.getBalance(wallets[4]);
+    await crowdsale.approveCustomer(wallets[3], {from: wallets[1]});
+    await increaseTimeTo(this.afterEnd);
+    await crowdsale.refund({from: wallets[3]}).should.be.rejectedWith(EVMRevert);
+    await crowdsale.refund({from: wallets[4], gasPrice: 0}).should.be.fulfilled;
+    const post = web3.eth.getBalance(wallets[4]);
+    post.minus(pre).should.be.bignumber.equal(ether(100));
   });
 
   it('should correctly calculate refund', async function () {
@@ -82,6 +96,7 @@ export default function (Token, Crowdsale, wallets) {
   it('should forward funds to wallets after end if goal was reached', async function () {
     const investment = this.softcap;
     await crowdsale.sendTransaction({value: investment, from: wallets[3]});
+    await crowdsale.approveCustomer(wallets[3], {from: wallets[1]});
     await increaseTimeTo(this.afterEnd);
     const pre = web3.eth.getBalance(wallets[2]);
     await crowdsale.finish({from: wallets[1]}).should.be.fulfilled;
