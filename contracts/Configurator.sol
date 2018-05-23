1551664f7b698e2bcd6bc937a79793f26d34005e
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-05-24 00:46:53 +0300

    chore(Configurator): remove all contract implementations

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -1,51 +1,106 @@
 pragma solidity ^0.4.18;
 
-import './ownership/Ownable.sol';
-import './MintableToken.sol';
-import './RomadDefenseToken.sol';
-import './PreICO.sol';
-import './ICO.sol';
-import './DoubleStageFreezeTokensWallet.sol';
+import "./ownership/Ownable.sol";
 
-contract Configurator is Ownable {
+contract RomadDefenseToken {
+  function setSaleAgent(address newSaleAgent) public;
+  function transferOwnership(address newOwner) public;
+}
 
-  MintableToken public token;
+contract PreICO {
+  function setWallet(address newWallet) public;
+  function setStart(uint newStart) public;
+  function addMilestone(uint period, uint bonus, uint minInvestedLimit) public;
+  function setUSDSoftcap(uint newUSDSoftcap) public;
+  function setUSDPrice(uint newUSDPrice) public;
+  function setETHtoUSD(uint newETHtoUSD) public;
+  function setMinInvestedLimit(uint newMinInvestedLimit) public;
+  function setNextSaleAgent(address newICO) public;
+  function setToken(address newToken) public;
+  function transferOwnership(address newOwner) public;
+}
 
+contract ICO {
+  function setWallet(address newWallet) public;
+  function setStart(uint newStart) public;
+  function addMilestone(uint period, uint bonus, uint minInvestedLimit) public;
+  function setUSDHardcap(uint newUSDHardcap) public;
+  function setUSDPrice(uint newUSDPrice) public;
+  function setETHtoUSD(uint newETHtoUSD) public;
+  function setMinInvestedLimit(uint newMinInvestedLimit) public;
+  function setTeamTokensWallet(address newTeamTokensWallet) public;
+  function setTeamTokensPercent(uint newTeamTokensPercent) public;
+  function setBountyTokensPercent(uint newBountyTokensPercent) public;
+  function setAdvisorsTokensWallet(address newAdvisorsTokensWallet) public;
+  function setAdvisorsTokensPercent(uint newAdvisorsTokensPercent) public;
+  function setEarlyInvestorsTokensWallet(address newEarlyInvestorsTokensWallet) public;
+  function setEarlyInvestorsTokensPercent(uint newBountyTokensPercent) public;
+  function setBountyTokensWallet (address newBountyWallet) public;
+  function setToken(address newToken) public;
+  function transferOwnership(address newOwner) public;
+}
+
+contract DoubleStageFreezeTokensWallet {
+  function setWallet(address newWallet) public;
+  function setMasterPercent(uint newPercent) public;
+  function setFirstDate(uint newFirstDate) public;
+  function setSecondDate(uint newSecondDate) public;
+  function activate() public;
+  function setToken(address newToken) public;
+  function transferOwnership(address newOwner) public;
+}
+
+contract TestConfigurator is Ownable {
+
+  RomadDefenseToken public token;
   PreICO public preICO;
-
   ICO public ico;
-
   DoubleStageFreezeTokensWallet public teamTokensWallet;
   DoubleStageFreezeTokensWallet public earlyInvestorsTokensWallet;
 
+  function setToken(address _token) public onlyOwner {
+    token = RomadDefenseToken(_token);
+  }
+
+  function setPreICO(address _preICO) public onlyOwner {
+    preICO = PreICO(_preICO);
+  }
+
+  function setICO(address _ico) public onlyOwner {
+    ico = ICO(_ico);
+  }
+
+  function setTeamTokensWallet(address _teamTokensWallet) public onlyOwner {
+    teamTokensWallet = DoubleStageFreezeTokensWallet(_teamTokensWallet);
+  }
+
+  function setEarlyInvestorsTokensWallet(address _earlyInvestorsTokensWallet) public onlyOwner {
+    earlyInvestorsTokensWallet = DoubleStageFreezeTokensWallet(_earlyInvestorsTokensWallet);
+  }
+
   function deploy() public onlyOwner {
 
-    token = new RomadDefenseToken();
-
-    preICO = new PreICO();
+    token.setSaleAgent(preICO);
 
     preICO.setWallet(0xa86780383E35De330918D8e4195D671140A60A74);
     preICO.setStart(1527440400); // 27 May 2018 17:00:00 GMT
     preICO.addMilestone(1, 20, 10000000000000000000); // 1 day, 20% bonus, 10 ETH min
     preICO.addMilestone(2, 18, 5000000000000000000); // 2 days, 20% bonus, 5 ETH min
-    preICO.addMilestone(4, 16, 1000000000000000000); // 4 days 16%, 1 ETH min
+    preICO.addMilestone(4, 16, 1000000000000000000); // 4 days, 16% bonus, 1 ETH min
     preICO.addMilestone(3, 15, 0);
     preICO.addMilestone(3, 14, 0);
     preICO.addMilestone(3, 13, 0);
     preICO.addMilestone(3, 12, 0);
     preICO.addMilestone(3, 11, 0);
-    preICO.setUSDPrice(200); // 0.2 USD
-    preICO.setUSDSoftcap(5000000000); //  5 000 000 USD
-    preICO.setETHtoUSD(67508); // 675.08 USD per ETH
+    preICO.setUSDPrice(200); // 1 RDT = 5 USD
+    preICO.setUSDSoftcap(5000000000); //  500 000 000 USD
+    preICO.setETHtoUSD(589330); // 1 ETH = 589.33 USD
     preICO.setMinInvestedLimit(100000000000000000); // 0.1 ETH fallback limit
     preICO.setToken(token);
-
-    token.setSaleAgent(preICO);
-
-    ico = new ICO();
+    preICO.setNextSaleAgent(ico);
 
     ico.setWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085);
-    ico.setStart(1529798400); // Jun 24 2018 00:00:00 GMT
+    ico.setStart(1529859600); // Jun 24 2018 17:00:00 GMT
     ico.addMilestone(6, 10, 0);
     ico.addMilestone(6, 9, 0);
     ico.addMilestone(6, 8, 0);
@@ -57,19 +112,20 @@ contract Configurator is Ownable {
     ico.addMilestone(6, 2, 0);
     ico.addMilestone(3, 1, 0);
     ico.addMilestone(3, 0, 0);
-    ico.setUSDPrice(200); // 0.2 USD
+    ico.setUSDPrice(200); // 1 RDT = 5 USD
     ico.setUSDHardcap(28000000000); // 28 000 000 USD
-    ico.setETHtoUSD(67508); // 675.08 USD per ETH
+    ico.setETHtoUSD(589330); // 1 ETH = 589.33 USD
     ico.setBountyTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
     ico.setBountyTokensPercent(5);
     ico.setTeamTokensPercent(10);
-    ico.setAdvisorsTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
+    ico.setAdvisorsTokensWallet(0xE7260D4c2a6539910d47F91c9060B4269dF2bD45);
     ico.setAdvisorsTokensPercent(5);
     ico.setEarlyInvestorsTokensPercent(15);
     ico.setMinInvestedLimit(100000000000000000); // 0.1 ETH fallback limit
     ico.setToken(token);
+    ico.setTeamTokensWallet(teamTokensWallet);
+    ico.setEarlyInvestorsTokensWallet(earlyInvestorsTokensWallet);
 
-    teamTokensWallet = new DoubleStageFreezeTokensWallet();
     teamTokensWallet.setMasterPercent(30);
     teamTokensWallet.setWallet(0x2AB0d2630eb67033E7D35eC1C43303a3F7720dA5);
     teamTokensWallet.setToken(token);
@@ -77,26 +133,20 @@ contract Configurator is Ownable {
     teamTokensWallet.setSecondDate(1567296000); // 01 Sep 2019 00:00:00 GMT
     teamTokensWallet.activate();
 
-    earlyInvestorsTokensWallet = new DoubleStageFreezeTokensWallet();
     earlyInvestorsTokensWallet.setMasterPercent(50);
-    earlyInvestorsTokensWallet.setWallet(0x2AB0d2630eb67033E7D35eC1C43303a3F7720dA5);
+    earlyInvestorsTokensWallet.setWallet(0xf3Eafc283C0fFa5C60206bd65D9753474c1aE48a);
     earlyInvestorsTokensWallet.setToken(token);
     earlyInvestorsTokensWallet.setFirstDate(1543622400); // 01 Dec 2018 00:00:00 GMT
     earlyInvestorsTokensWallet.setSecondDate(1567296000); // 01 Sep 2019 00:00:00 GMT
     earlyInvestorsTokensWallet.activate();
 
-    ico.setTeamTokensWallet(teamTokensWallet);
-    ico.setEarlyInvestorsTokensWallet(earlyInvestorsTokensWallet);
-
-    preICO.setNextSaleAgent(ico);
-
     address manager = 0x675eDE27cafc8Bd07bFCDa6fEF6ac25031c74766;
 
-    teamTokensWallet.transferOwnership(manager);
-    earlyInvestorsTokensWallet.transferOwnership(manager);
     token.transferOwnership(manager);
     preICO.transferOwnership(manager);
     ico.transferOwnership(manager);
-  }
+    teamTokensWallet.transferOwnership(manager);
+    earlyInvestorsTokensWallet.transferOwnership(manager);
 
+  }
 }
